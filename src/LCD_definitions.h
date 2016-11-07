a8c44822593fe58c6963cd5577933641e1a07252
kitusmark <electronicslab@icloud.com>
2016-11-07 15:20:52 +0100

    Added 4D Systems uLCD display support

---
+++ src/LCD_definitions.h
@@ -0,0 +1,20 @@
+
+// LCD_definitions.h
+// All the definitions and constants related to items in the LCD
+// Created: 07/11/2016 15:05:00
+// Author: Marc Cobler Cosmen (kitusmark)
+// https://github.com/kitusmark/waterReservoirControl
+
+#pragma region FORMS
+
+// Template :
+//
+// #define FORM #formNumber
+//   #define ELEMENT #elementNumber
+//   ...
+//
+
+
+
+
+#pragma endregion FORMS
